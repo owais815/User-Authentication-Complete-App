02b3eb61fd3855748f989933e01ececf7900650b
Owais Mehmood <20-arid-815@student.uaar.edu.pk>
2024-07-15 22:53:56 +0500

    final commit

---
+++ frontend/src/Components/RefreshHandler.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+function RefreshHandler({ setIsAuthenticated }) {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setIsAuthenticated(true);
+      if (
+        location.pathname === "/" ||
+        location.pathname === "/login" ||
+        location.pathname === "/signup"
+      ) {
+        navigate("/home", { replace: true });
+      } else if (location.state && location.state.from) {
+        navigate(location.state.from, { replace: true });
+      }
+    }
+  }, [location, navigate, setIsAuthenticated]);
+
+  return null;
+}
+
+export default RefreshHandler;
+ 
